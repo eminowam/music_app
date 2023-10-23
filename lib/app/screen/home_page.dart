3a863f491550f2f9b64d1f9d51e2cc15d9cc8113
aimeerimeminova <aimemynoo@gmail.com>
2023-10-23 15:11:53 +0600

    Add bottom navigation bar

--- lib/app/screen/home_page.dart
+++ lib/app/screen/home_page.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:music_app/app/screen/favorite_screen.dart';
+import 'package:music_app/app/screen/page_one.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 3,
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "Home")
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+      body: Builder(
+        builder: (context) {
+          if (_currentIndex == 0) {
+            return PageOne();
+          } else if (_currentIndex == 1) {
+            return FavoriteScreen();
+          }
+          return SizedBox();
+        },
+      ),
     );
   }
 }
